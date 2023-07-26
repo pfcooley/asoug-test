97fd8b09d9f8bed35d1001a687806e62c41fde64
pfcooley <45568838+pfcooley@users.noreply.github.com>
2023-07-26 13:17:24 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ATLAS Satellite Operator User Guide'
+copyright = '2023, ATLAS Space Operations'
+author = 'ATLAS Space Operations'
 
 release = '0.1'
 version = '0.1.0'
